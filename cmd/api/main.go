5574031bd490ec1207cc3d95ae8d5404aefaa930
Kyrylo Baranov <noemail>
2026-07-08 21:20:18 +0300

    feat(api): implement health check endpoint and load environment variables

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -3,4 +3,24 @@ package main
 import (
 	"log"
 	"net/http"
+	"strconv"
+
+	"github.com/joho/godotenv"
+	"github.com/smithautotest/clinic-visits/internal/config"
 )
+
+func main() {
+	godotenv.Load()
+	config, err := config.LoadConfig()
+	if err != nil {
+		log.Fatal("Error loading config:", err)
+	}
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		_, _ = w.Write([]byte("OK"))
+	})
+
+	log.Fatal(http.ListenAndServe(":"+strconv.Itoa(config.HTTPPort), mux))
+}
